92429194662f85b1234612c9f63f0e6caab505b1
zhangli <945560537@qq.com>
2018-01-15 19:43:10 +0800

    add more debug info

--- Network.py
+++ Network.py
@@ -8,36 +8,40 @@ class Network:
         self.resolution = resolution
         self.train_batch_size = batch_size
         self.trace_length_size = trace_length
-        print("--------------------------checkpoint 0")
 
         self.state = tf.placeholder(tf.float32, shape=[None, resolution[0], resolution[1], resolution[2]])
-        print("--------------------------checkpoint 1")
 
         conv1 = slim.conv2d(inputs=self.state, num_outputs=32, kernel_size=[8, 8], stride=[4, 4],
                             activation_fn=tf.nn.relu, padding='VALID', scope=scope+'_c1')
-        print("--------------------------checkpoint 2")                    
 
         conv2 = slim.conv2d(inputs=conv1, num_outputs=64, kernel_size=[4, 4], stride=[2, 2],
                             activation_fn=tf.nn.relu, padding='VALID', scope=scope+'_c2')
-        print("--------------------------checkpoint 3")                    
 
         conv3 = slim.conv2d(inputs=conv2, num_outputs=64, kernel_size=[3, 3], stride=[1, 1],
                             activation_fn=tf.nn.relu, padding='VALID', scope=scope+'_c3')
-        print("--------------------------checkpoint 4")                    
 
         flat = slim.flatten(conv3)
+        print("--------------------------checkpoint 0")
 
         self.cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_size, state_is_tuple=True)
+        print("--------------------------checkpoint 1")
         self.train_length = tf.placeholder(dtype=tf.int32)
+        print("--------------------------checkpoint 2")
         self.batch_size = tf.placeholder(dtype=tf.int32)
+        print("--------------------------checkpoint 3")
 
         self.fc_reshape = tf.reshape(flat, [self.batch_size, self.train_length, hidden_size])
+        print("--------------------------checkpoint 4")
         self.state_in = self.cell.zero_state(self.batch_size, tf.float32)
+        print("--------------------------checkpoint 5")
         self.rnn, self.rnn_state = tf.nn.dynamic_rnn(inputs=self.fc_reshape, cell=self.cell, dtype=tf.float32,
                                                      initial_state=self.state_in, scope=scope+'_rnn')
+        print("--------------------------checkpoint 6")                                             
         self.rnn = tf.reshape(self.rnn, shape=[-1, hidden_size])
+        print("--------------------------checkpoint 7")
 
         self.q = slim.fully_connected(self.rnn, action_count, activation_fn=None)
+        print("--------------------------checkpoint 8")
 
         self.best_a = tf.argmax(self.q, 1)
 
@@ -45,7 +49,7 @@ class Network:
         self.actions = tf.placeholder(shape=[None], dtype=tf.int32)
         self.actions_onehot = tf.one_hot(self.actions, action_count, dtype=tf.float32)
         self.q_chosen = tf.reduce_sum(tf.multiply(self.q, self.actions_onehot), axis=1)
-
+        print("--------------------------checkpoint 9")
         self.loss = tf.losses.mean_squared_error(self.q_chosen, self.target_q)
 
         self.optimizer = tf.train.RMSPropOptimizer(learning_rate=lr, momentum=0.95, epsilon=0.01)
