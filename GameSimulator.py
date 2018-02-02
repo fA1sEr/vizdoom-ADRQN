f14e007488c26ad4b855f1242c7c3d76fef2b246
zhangli <945560537@qq.com>
2018-02-02 20:37:50 +0800

    ADRQN-DOOM BETAAAAAA Ver.

--- GameSimulator.py
+++ GameSimulator.py
@@ -1,8 +1,9 @@
-from ale_python_interface import ale_python_interface
-
+from vizdoom import *
 import skimage.color
 import skimage.transform
 import numpy as np
+
+scenario_path = "/home/ghmiao/VizDoomDependFiles/ViZDoom/scenarios/simpler_basic.cfg" # Name and path of scenario
 
 class GameSimulator:
     def __init__(self, frame_repeat=4, resolution=(80, 45, 3)):
@@ -13,20 +14,18 @@ class GameSimulator:
         self.rewards = 0.0
         self.last_action = 0
         
-    def initialize(self, visiable=False, rom=b'pong.bin'):
-        # 初始化游戏，返回游戏的动作数目
-        print("Initializing Atari...")
-        self.game = ale_python_interface.ALEInterface()
-        self.game.setBool(b"display_screen", visiable)
-        self.game.setBool(b"sound", visiable)
-        self.game.loadROM(rom)
-        # 获取屏幕大小
-        self.screen_width, self.screen_height = self.game.getScreenDims()
-        # 初始化动作 one_hot Actions:[0 3 4]
-        self.actions = self.game.getMinimalActionSet()
-        print('Actions:', self.actions)
-        print("Atari initialized.")
-        return len(self.actions)
+    def initialize(self):
+        print("Initializing doom...")
+        self.game = DoomGame()
+        self.game.load_config(scenario_path)
+        self.game.set_window_visible(False)
+        self.game.set_mode(Mode.PLAYER)
+        self.game.set_screen_format(ScreenFormat.RGB24)
+        self.game.set_screen_resolution(ScreenResolution.RES_400X225)
+        self.game.init()
+        n = self.game.get_available_buttons_size()
+        self.actions = [list(a) for a in it.product([0, 1], repeat=n)]
+        print("Doom initialized.")
     
     # 对游戏图像进行处理，后期可能放到GameSimulator中
     def __preprocess(self, img):
@@ -36,9 +35,9 @@ class GameSimulator:
     
     def get_state(self, preprocess=True):
         # 获取当前游戏的画面，游戏结束则获得空
-        if self.is_terminared():
+        if self.game.is_episode_finished():
             return None
-        img = self.game.getScreenRGB()
+        img = self.game.get_state()
         #img = img.reshape([self.screen_height, self.screen_width])
         # 如果进行预处理
         if preprocess: img = self.__preprocess(img)
@@ -46,37 +45,25 @@ class GameSimulator:
     
     def get_action_size(self):
         # 获取动作数目
-        return len(self.game.getMinimalActionSet())
+        return self.game.get_available_buttons_size()
     
     def make_action(self, action):
-        # 重复执行动作
-        immediate_score = 0.0
-        for i in range(self.frame_repeat):
-            immediate_score += self.game.act(self.actions[action])
-            if self.is_terminared(): break
-        reward = immediate_score/np.abs(immediate_score) if immediate_score!=0.0 else 0.0
+        reward = self.game.make_action(actions[action], FRAME_REPEAT)
         new_state = self.get_state()
-        done = self.is_terminared()
+        done = self.is_episode_finished()
         self.rewards += reward
         last_action = self.last_action
         self.last_action = action
         return last_action, new_state, reward, done
     
-    def is_terminared(self):
+    def is_episode_finished(self):
         # 判断游戏是否终止
-        return self.game.game_over()
+        return self.game.is_episode_finished()
     
     def reset(self):
         # 重新开始游戏
-        self.game.reset_game()
+        self.game.new_episode()
         self.rewards = 0.0
-    
-    def close(self):
-        # 关闭游戏模拟器
-        self.game.close()
         
     def get_total_reward(self):
         return self.rewards
-
-    def get_last_action(self):
-        return self.last_action
